b6cf528a790816d5510e4632c0f510c013de5e97
Md. Nayem Hossain <nayem.hossain7298@gmail.com>
2023-07-25 23:50:50 +0600

    table added

--- src/components/ExpenseTransactions.jsx
+++ src/components/ExpenseTransactions.jsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import AppLayout from '../layout/AppLayout';
 
 const ExpenseTransactions = () => {
-    const { expenseList, setExpenseList } = useState([]);
+    const [inputAmount, setInputAmount] = useState('');
+    const [tableRowList, setTableRowList] = useState([]);
 
     const getTodaysDate = () => {
         const now = new Date();
@@ -11,59 +12,72 @@ const ExpenseTransactions = () => {
         const date = String(now.getDate()).padStart(2, '0');
         const formattedDate = `${year}-${month}-${date}`;
         return formattedDate;
-      }
+    }
 
-      const getCurrentTime = () => {
+    const getCurrentTime = () => {
         const now = new Date();
         const options = {
-          hour: '2-digit',
-          minute: '2-digit',
-          second: '2-digit',
-          hour12: true,
-          timeZone: 'Asia/Dhaka',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: true,
+            timeZone: 'Asia/Dhaka',
         };
-      
+
         const formattedTime = now.toLocaleString('en-BD', options);
         return formattedTime;
-      }
-const getExpenseRow=()=>{
-  
-}
+    }
+    const generateTableRow = () => {
+        const nextID = tableRowList.length === 0 ? 1 : tableRowList[tableRowList.length - 1].sl + 1;
+        tableRowList.push({
+            sl: nextID,
+            date: getTodaysDate(),
+            inputTime: getCurrentTime(),
+            expenseAmount: inputAmount,
+        });
+
+        setTableRowList([...tableRowList]);
+        setInputAmount('');
+    }
     return (
         <AppLayout>
             <div>
-                <form onSubmit={""}>
-                    <div className="flex justify-center">
-                        <input type="text" placeholder="Input your expense..." className="input input-bordered w-full max-w-xs" />
-                        <button className="btn btn-accent">Submit</button>
-                    </div>
-                </form>
+
+                <div className="flex justify-center">
+                    <input onChange={(e) => setInputAmount(e.target.value)} value={inputAmount} type="text" placeholder="Input your expense..." className="input input-bordered w-full max-w-xs" />
+                    <button onClick={generateTableRow} className="btn btn-accent">Submit</button>
+                </div>
+
 
                 <div className="overflow-x-auto mt-10">
                     <table className="table table-xs">
                         <thead>
                             <tr>
                                 <th>SL</th>
-                                <th>date</th>
+                                <th>Date</th>
                                 <th>Input Time</th>
                                 <th>Expense Amount</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <th>{1}</th>
-                                <td> </td>
-                                <td> </td>
-                                <td> </td>
-                            </tr>
-                            
+                            {
+                                tableRowList.length !== 0 ? (
+                                    tableRowList.map((item, index) => {
+                                        return (
+                                            <tr>
+                                                <th>{item.sl}</th>
+                                                <td>{item.date}</td>
+                                                <td>{item.inputTime}</td>
+                                                <td>{item.expenseAmount}</td>
+                                            </tr>
+                                        );
+                                    })
+                                ) : (<></>)
+                            }
                         </tbody>
                         <tfoot>
                             <tr>
-                                <th>Total Expense</th>
-                                <th></th>
-                                <th></th>
-                                <th></th>
+                               
                             </tr>
                         </tfoot>
                     </table>
--- src/components/IncomeTransactions.jsx
+++ src/components/IncomeTransactions.jsx
@@ -1,16 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppLayout from '../layout/AppLayout';
 
 const IncomeTransactions = () => {
+
+    const [inputAmount, setInputAmount] = useState('');
+    const [tableRowList, setTableRowList] = useState([]);
+
+    const getTodaysDate = () => {
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = String(now.getMonth() + 1).padStart(2, '0');
+        const date = String(now.getDate()).padStart(2, '0');
+        const formattedDate = `${year}-${month}-${date}`;
+        return formattedDate;
+    }
+
+    const getCurrentTime = () => {
+        const now = new Date();
+        const options = {
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: true,
+            timeZone: 'Asia/Dhaka',
+        };
+
+        const formattedTime = now.toLocaleString('en-BD', options);
+        return formattedTime;
+    }
+    const generateTableRow = () => {
+        const nextID = tableRowList.length === 0 ? 1 : tableRowList[tableRowList.length - 1].sl + 1;
+        tableRowList.push({
+            sl: nextID,
+            date: getTodaysDate(),
+            inputTime: getCurrentTime(),
+            expenseAmount: inputAmount,
+        });
+
+        setTableRowList([...tableRowList]);
+        setInputAmount('');
+    }
     return (
         <AppLayout>
             <div>
-                <form action="">
-                    <div className="flex justify-center">
-                        <input type="text" placeholder="Input your income..." className="input input-bordered w-full max-w-xs" />
-                        <button className="btn btn-accent">Submit</button>
-                    </div>
-                </form>
+
+                <div className="flex justify-center">
+                    <input onChange={(e) => setInputAmount(e.target.value)} value={inputAmount} type="text" placeholder="Input your income..." className="input input-bordered w-full max-w-xs" />
+                    <button onClick={generateTableRow} className="btn btn-accent">Submit</button>
+                </div>
+
 
                 <div className="overflow-x-auto mt-10">
                     <table className="table table-xs">
@@ -23,13 +61,20 @@ const IncomeTransactions = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <th>{1}</th>
-                                <td> </td>
-                                <td> </td>
-                                <td> </td>
-                            </tr>
-                            
+                            {
+                                tableRowList.length !== 0 ? (
+                                    tableRowList.map((item, index) => {
+                                        return (
+                                            <tr>
+                                                <th>{item.sl}</th>
+                                                <td>{item.date}</td>
+                                                <td>{item.inputTime}</td>
+                                                <td>{item.expenseAmount}</td>
+                                            </tr>
+                                        );
+                                    })
+                                ) : (<></>)
+                            }
                         </tbody>
                         <tfoot>
                             <tr>
